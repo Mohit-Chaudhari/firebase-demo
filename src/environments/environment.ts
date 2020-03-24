8f10f22664e11e403d5c5ee84da3cfc0293f8dbd
Mohit <chaudharimohit39@gmail.com>
2020-03-25 00:17:21 +0530

    Added firebase config key in Angular project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDjOV7LmQf_yTlCgG7j1L3S-8P4CfG6jwY",
+    authDomain: "fir-demo-ad5a4.firebaseapp.com",
+    databaseURL: "https://fir-demo-ad5a4.firebaseio.com",
+    projectId: "fir-demo-ad5a4",
+    storageBucket: "fir-demo-ad5a4.appspot.com",
+    messagingSenderId: "74160814359",
+    appId: "1:74160814359:web:4b735733f5dfb668075f52"
+  }
 };
 
 /*
