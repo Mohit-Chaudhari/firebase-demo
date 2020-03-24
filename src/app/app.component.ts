4a5cb04cab6e73fe03d49ff4b9b52cd435197bce
Mohit <chaudharimohit39@gmail.com>
2020-03-25 01:07:27 +0530

    Added reactive form module dependncy in app.module.ts and added angularfire2 dependency in app.component.ts file and initialize it in constructor of same file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireList,AngularFireDatabase } from 'angularfire2/database';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private firebase: AngularFireDatabase) { }
+  NameList: AngularFireList<any>;
+
   title = 'Firebase Demo';
 }
